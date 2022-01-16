47349f859dc27e535a89bdba46f53e227f08fd25
dispersion <anyta1901@gmail.com>
2022-01-16 17:50:18 +0300

    Коммит во второй ветке с исправленным решением

--- src/ru/spypro/homework/Main.java
+++ src/ru/spypro/homework/Main.java
@@ -18,7 +18,7 @@ public class Main {
 	// write your code here
         //int[] arr = generateRandomArray();
        // exerciseOne();
-        //exerciseTwo();
+        exerciseTwo();
         //exerciseThree();
         //exerciseFour();
         }
@@ -27,7 +27,7 @@ public class Main {
         System.out.println("Задача 1: ");
         int arraySum = 0;
         int[] arr = generateRandomArray();
-        for(int a = 0; a < arr.length; a++ ) {
+        for (int a = 0; a < arr.length; a++) {
             arraySum = arraySum + arr[a];
         }
         System.out.println("Сумма трат за месяц составила " + arraySum + " рублей");
@@ -36,26 +36,35 @@ public class Main {
     public static void exerciseTwo() { // максимальное и минимальное число
         System.out.println("Задача 2: ");
         int[] array = generateRandomArray();
+        // обозначение переменных которые должны работать для разных массивов
         int min = array[0];
         int max = array.length - 1;
+        /*  обозначение по примеры 14 минуты лекции урока массив ч2
+        int min = 300_00;
+        int max = - 1;*/
+
         for (int i = 0; i < array.length; i++) {
             if (array[i] < min) {
                 min = array[i];
-                System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+
             } else {
                 if (array[i] > max) {
                     max = array[i];
-                    System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+
                 }
             }
         }
+
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+
     }
 
     public static void exerciseThree(){ //средняя сумма за 30 дней
         System.out.println("Задача 3: ");
         int[] averageArray = generateRandomArray();
         double generalSum = 0;
-        for(int i =0; i<averageArray.length; i++){
+        for (int i = 0; i < averageArray.length; i++) {
             generalSum = generalSum + averageArray[i];
 
         }
@@ -65,8 +74,8 @@ public class Main {
 
     public static void exerciseFour(){  // в обратном порядке массив
         System.out.println("Задача 4: ");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length-1; i>=0; i--){
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
         }
 
